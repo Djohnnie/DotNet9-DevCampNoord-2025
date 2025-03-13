e52ecfae36a511cdc10165be1b49e8814897e5e7
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2025-03-13 20:01:59 +0100

    Initial commit of .NET 9 and C# 13 examples

---
+++ CSharp13.ImplicitIndexAccess/Program.cs
@@ -0,0 +1,45 @@
+﻿
+var dataBuffer = new Buffer<int>(10);
+dataBuffer[^1] = 1;
+
+var buffer = new Buffer<int>(10)
+{
+    [^1] = 1,
+    [^2] = 2,
+    [^3] = 3,
+    [^4] = 4,
+    [^5] = 5,
+    [^6] = 6,
+    [^7] = 7,
+    [^8] = 8,
+    [^9] = 9,
+    [^10] = 10
+};
+
+
+Console.ReadKey();
+
+
+public class Buffer<T>
+{
+    private T[] buffer;
+
+    public Buffer(int size)
+    {
+        buffer = new T[size];
+    }
+
+    public T this[Index index]
+    {
+        get => buffer[index];
+        set => buffer[index] = value;
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            Console.WriteLine($"[{i}] = {buffer[i]}");
+        }
+    }
+}
---
+++ DotNet9.BinaryFormatterRemovel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+BinaryFormatter formatter = new BinaryFormatter();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+
+try
+{
+    var person = new Person
+    {
+        FirstName = "John",
+        LastName = "Doe",
+        Age = 25
+    };
+
+    using var memoryStream = new MemoryStream();
+    formatter.Serialize(memoryStream, person);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Failed to serialize: {ex.Message}");
+}
+
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
